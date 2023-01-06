e6e8de4aa6e2c84ebc9a0b53280f65745d4752f8
R0g3r-SH <Rogelio84@hotmail.com>
2023-01-06 12:12:36 -0600

    houu yeahh

---
+++ Algorithm II/438. Find All Anagrams in a String.py
@@ -0,0 +1,24 @@
+def findAnagrams( s, p):
+    if len(p) > len(s): return []
+    dic1,dic2 = {},{}
+
+    for i in range(len(p)):
+        dic1[s[i]] = dic1.get(s[i],0)+1
+        dic2[p[i]] = dic2.get(p[i],0)+1
+    
+    res = [0] if dic1 == dic2 else []
+
+    l=0
+
+    for i in range(len(p),len(s)):
+        dic1[s[i]] = dic1.get(s[i],0)+1
+        dic1[s[l]] -= 1
+
+        if dic1[s[l]] == 0:
+            dic1.pop(s[l])
+        l+=1
+        if dic1 == dic2:
+            res.append(l)
+
+    return res
+            
